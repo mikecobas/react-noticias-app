2b5c0f608afd18d5eaa89cff7c5091997a746295
Miguel Cobas <mcobas@dealergeek.com>
2020-04-18 02:34:45 -0500

    Se creo la consulta al api y un state para guardar las noticias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
@@ -7,6 +7,23 @@ function App() {
   // definir categoria y noticias
 
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias ] = useState([]);
+
+
+  useEffect(() =>{
+
+    const consultarAPI = async () =>{
+      const APIKEY = '7c7fa6171772477783eb844a64e9bd64'
+
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${APIKEY}`
+
+        const respuesta = await fetch(url);
+        const noticias = await respuesta.json();
+        guardarNoticias(noticias.articles);
+    }
+
+    consultarAPI()
+  }, [categoria])
 
   return (
     <Fragment>
